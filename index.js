e7f18eb9347c12382f088552288757ef7c0fcffa
Justin Goping <jgoping@gmail.com>
2021-02-28 16:08:51 -0500

    feat: instantiate RuntimeClientFactory

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 const config = require('./config.json');
+const RuntimeClientFactory = require("@voiceflow/runtime-client-js");
 
 const client = new Discord.Client();
 
